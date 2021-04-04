7dfac5266ddf89c4529f51d76ca7773a5b12e437
MikeMayer <mike@mikemayer.me>
2021-04-03 23:41:12 -0700

    Adds async checksum method

--- utility.js
+++ utility.js
@@ -1,9 +1,16 @@
+const crypto = require('crypto')
+const fs = require('fs')
+
+/**
+ * @param {Array<any>} array
+ */
 const random = array => array[Math.floor(Math.random() * array.length)]
 
 /**
  * x ∪ y
  */
 const union = (x, y) => [...new Set([...x, ...y])]
+
 /**
  * x ∩ y
  */
@@ -14,8 +21,24 @@ const intersection = (x, y) => {
     return new Set([...x].filter(i => y.has(i)))
 }
 
+/**
+ * @param {String} path absolute path to a file to be hashed
+ * @returns {Promise<String>} hexadecimal hash string of file
+ */
+const checksum = async (path, hashAlgorithm = 'sha1') => {
+    return new Promise((resolve, reject) => {
+        const hash = crypto.createHash(hashAlgorithm)
+        const input = fs.createReadStream(path)
+
+        input.on('error', reject)
+        input.on('data', chunk => { hash.update(chunk) })
+        input.on('close', () => { resolve(hash.digest('hex')) })
+    })
+}
+
 module.exports = {
     random,
     union,
-    intersection
+    intersection,
+    checksum
 }
